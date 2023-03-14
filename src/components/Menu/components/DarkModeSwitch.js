587dd97e15fdcf3d19b13a691969d065cb0d677c
Millene Ishiyama <milleneeishiyama@gmail.com>
2023-03-14 09:27:10 -0700

    Organizing code

--- src/components/Menu/components/DarkModeSwitch.js
+++ src/components/Menu/components/DarkModeSwitch.js
@@ -51,10 +51,7 @@ export default function DarkModeSwitch() {
     const contexto = React.useContext(ColorModeContext)
     return (
         <StyledSwitch>
-            <input id="darkmode" type="checkbox" onChange={ () => {
-                contexto.toggleMode()
-            } } 
-            />
+            <input id="darkmode" type="checkbox" onChange={ () => contexto.toggleMode() } />
             <label htmlFor="darkmode" className="darkmode-switch">
                 <span>
                     🌙
